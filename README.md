f286ddf61fb8b024653ae324c354f34c4587ac6d
Marco Samano <antoniost.developer@gmail.com>
2019-05-07 13:07:48 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+TokenJwt  
+===============
+
+Libreria para generar tokens con JWT (Firebase)
+
+## Instalar
+```
+composer require antoniosam/tokenjwt
+```
+
+##Uso
+
+**$data Debe ser arreglo** 
+
+$data = [ 'id' => 1 ];
+```
+ $data = ['id'=>1];
+ $tmp = new TokenJwt($key,$duration,$timeoffset);
+ $token = $tmp->generate($data);
+ if($obj->validate($token)){
+    $info_decode  = $obj->getDatadecode();
+ }else{
+    echo $obj->getErrorDecode();
+ }
+
+```
+## Metodos staticos
+
+```
+TokenJwt::generateToken($secret_key,$data);
+$info_decode = TokenJwt::validaToken($secret_key,$jwt);
+
+if(!is_null($info_decode)){
+//success decode
+}else{
+//error token
+}
+```
---
+++ src/TokenJwt.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Ast\TokenJwt;
+
+use Firebase\JWT\JWT;
+
+class TokenJwt
+{
+    private $key ;
+    private $duration = 3600;
+    private $offset = 0;
+
+    private $datadecode;
+    private $error;
+    private $decodesuccess = false;
+
+    function __construct($key, $duration=null, $timeoffset = null  )
+    {
+        $this->key = $key;
+        if(!is_null($duration)){
+            $this->duration = $duration;
+        }
+        if(!is_null($timeoffset)){
+            $this->offset = $timeoffset;
+        }
+    }
+
+    /**
+     * @param array $data
+     * @return string
+     */
+    public function generate($data){
+        $token = [
+            'iat' => time() + $this->offset,
+            'exp' => time() + $this->duration,
+            'data' => $data
+        ];
+        return JWT::encode($token, $this->key);
+    }
+
+    /**
+     * @param $jwt
+     * @return bool
+     */
+    public function validate($jwt){
+        try{
+            $decode = JWT::decode($jwt, $this->key, ['HS256']);
+            $this->datadecode = $decode->data;
+            $this->decodesuccess = true;
+            return true;
+        }catch (\Exception $e){
+            $this->error = $e->getMessage();
+            return false;
+        }
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getDatadecode()
+    {
+        if($this->decodesuccess){
+            return $this->datadecode;
+        }
+        return null;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getErrorDecode()
+    {
+        return $this->error;
+    }
+
+
+
+    /**
+     * @param $key
+     * @param $data
+     * @param null $duration
+     * @return string
+     */
+    public static function generateToken($key,$data,$duration = null){
+        $obj = new self($key,$duration);
+        return $obj->generate($data);
+    }
+
+    /**
+     * @param $key
+     * @param $jwt
+     * @return null
+     */
+    public static function validaToken($key,$jwt){
+        $obj = new self($key);
+        if($obj->validate($jwt)){
+            return $obj->getDatadecode();
+        }else{
+            return null;
+        }
+    }
+
+}
